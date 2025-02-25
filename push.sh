d2faaf1d19a4b55b1e1728de76de9e8408f47fd4
seydi-ahmed <mouhamedcr8diouf@gmail.com>
2025-02-25 16:43:26 +0000

    le comment du pourquoi

--- push.sh
+++ push.sh
@@ -2,5 +2,5 @@ git config --global user.email "seydiahmedelcheikh@gmail.com"
 git config --global user.name "mouhameddiouf"
 git config credential.helper store
 git add .
-git commit -m "interligne"
+git commit -m "le comment du pourquoi"
 git push --force
